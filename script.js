0cac078291b344e22ed93cfe1c518223849a7365
Nyto-bg <tonyhm9416@gmail.com>
2026-01-14 15:32:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Changement de style du menu au scroll
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-// Menu mobile (Hamburger)
 function toggleMenu(){
     const menuToggle = document.querySelector('.menuToggle');
     const navbar = document.querySelector('.navbar');
